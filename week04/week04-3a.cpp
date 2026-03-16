16e8b06b4dd2547f31ad76654373d92794f7443b
Jeng-Sheng Yeh 葉正聖 <jsyeh.org@gmail.com>
2026-03-16 11:59:48 +0800

    week04

---
+++ week04/week04-3a.cpp
@@ -0,0 +1,30 @@
+// week04-3.cpp SOIT106_BASE_010
+#include <stdio.h>
+int main()
+{
+	int ans = 0;
+	int now;
+
+	scanf("%d", &now);
+	if (now%3==0) ans++;
+	scanf("%d", &now);
+	if (now%3==0) ans++;
+	scanf("%d", &now);
+	if (now%3==0) ans++;
+	scanf("%d", &now);
+	if (now%3==0) ans++;
+	scanf("%d", &now);
+	if (now%3==0) ans++;
+	scanf("%d", &now);
+	if (now%3==0) ans++;
+	scanf("%d", &now);
+	if (now%3==0) ans++;
+	scanf("%d", &now);
+	if (now%3==0) ans++;
+	scanf("%d", &now);
+	if (now%3==0) ans++;
+	scanf("%d", &now);
+	if (now%3==0) ans++;
+
+	printf("%d\n", ans);
+}
